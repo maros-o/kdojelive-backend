ce91161fee73e82c9a6ec5c18ed89e02a407b4c6
maros-o <maros.mec@seznam.cz>
2023-05-29 21:06:30 +0200

    auto commit

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ def get_streams():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
