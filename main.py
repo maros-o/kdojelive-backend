9503edd55d9999c3aace727e5d2ff1b0a5a11ba6
maros-o <maros.mec@seznam.cz>
2023-06-14 16:50:52 +0200

    remove trovo

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def update_streams():
 
         new_streams += youtube.get_streams()
 
-        trovo.update_trovo_streams()
-        new_streams += trovo.get_streams()
+        #trovo.update_trovo_streams()
+        #new_streams += trovo.get_streams()
 
         new_streams.sort(key=lambda x: x['viewer_count'], reverse=True)
 
